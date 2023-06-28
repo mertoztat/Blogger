5502cf53319fff387b1a35f922b7357111a8e0a2
Mert Öztat <mertoztat@gmail.com>
2023-06-28 13:51:06 +0300

    feat: connected to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to mongoDB"))
+  .catch((err) => console.log(err));
 
 app.listen("5000", () => {
   console.log("Backend is running 5000");
