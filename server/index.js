c9bb7d002913c0371465d9e4f36132fb14ac47b1
Mert Öztat <mertoztat@gmail.com>
2023-06-29 13:52:11 +0300

    feat: image upload multer

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const { default: mongoose } = require("mongoose");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -19,9 +21,24 @@ mongoose
   .then(console.log("connected to mongoDB"))
   .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/server/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/server/auth", authRoute);
 app.use("/server/users", usersRoute);
 app.use("/server/posts", postRoute);
+app.use("/server/categories", categoryRoute);
 
 // Server Running Log
 app.listen("5000", () => {
